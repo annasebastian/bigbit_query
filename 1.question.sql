a02dee9ccd1525365c97b0d00f3277e238e0abe9
annasebastian <100956157+annasebastian@users.noreply.github.com>
2022-03-05 07:01:10 -0800

    Update and rename crime data.sql to 1.question.sql

--- 1.question.sql
+++ 1.question.sql
@@ -1,4 +1,4 @@
-#2.	List out the crimes that are not yet cleared 
+#.	List out the crimes that are not yet cleared 
 SELECT description 	
 ,clearance_status
 FROM `bigquery-public-data.austin_crime.crime`
