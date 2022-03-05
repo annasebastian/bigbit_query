b69c7c8946f3621febe03091a0e9961510483300
annasebastian <100956157+annasebastian@users.noreply.github.com>
2022-03-05 06:39:39 -0800

    Update 4.question 4.sql

--- 4.question 4.sql
+++ 4.question 4.sql
@@ -1,4 +1,4 @@
-#3.	Find out the maximum load weight of each load type from Austin public data related to waste
+#.	Find out the maximum load weight of each load type from Austin public data related to waste
 SELECT load_type,
  max (load_weight)
  FROM `bigquery-public-data.austin_waste.waste_and_diversion`
